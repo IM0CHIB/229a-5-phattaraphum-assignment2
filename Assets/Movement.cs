8718bdf9306364330054d81d610bc371e6413216
IM0CHIB <mochimeeudon@gmail.com>
2024-03-06 21:08:27 +0700

    Airplane
    final!!!

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -4,28 +4,22 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // ความเร็วในการเคลื่อนที่
     public float moveSpeed = 5.0f;
 
-    // Rigidbody ของ Object
     private Rigidbody rb;
 
     void Start()
     {
-        // เก็บ Rigidbody ของ Object ไว้
         rb = GetComponent<Rigidbody>();
     }
 
     void FixedUpdate()
     {
-        // รับค่า Input จากคีย์บอร์ด
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
-
-        // คำนวณทิศทางการเคลื่อนที่
+        
         Vector3 moveDirection = new Vector3(horizontalInput, 0.0f, verticalInput);
-
-        // เคลื่อนที่ Object โดยใช้ Rigidbody
+        
         rb.AddForce(moveDirection * moveSpeed);
     }
 }
